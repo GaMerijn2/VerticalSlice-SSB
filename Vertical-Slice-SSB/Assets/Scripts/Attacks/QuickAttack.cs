c160d1301202b24f78bece1b2f9bf612d396cd2a
TimothyKuijper <t.kuijper12@gmail.com>
2023-11-21 13:10:28 +0100

    started attacks

---
+++ Vertical-Slice-SSB/Assets/Scripts/Attacks/QuickAttack.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuickAttack : MonoBehaviour
+{
+    [SerializeField]private string QuickAttackAxis;
+    [SerializeField]private string HeavyAttackAxis;
+
+    //player 1 controlls: "C" for quick attack and "V" for heavy attack
+
+    //player 2 controlls: "O" for quick attack and "P" for heavy attack 
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
