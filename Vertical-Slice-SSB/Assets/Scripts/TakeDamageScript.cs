023260bf1fb4161f582be6edc79622e81e481098
TrungToan1 <34772@ma-web.nl>
2023-11-22 12:10:19 +0100

    Added knockback system (unfinished)

---
+++ Vertical-Slice-SSB/Assets/Scripts/TakeDamageScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TakeDamageScript : MonoBehaviour
+{
+  
+    public string keybind; //Assign keybind in inspector
+    [SerializeField] PlayerHealthScript Health; //Player health
+    [SerializeField] PlayerKnockbackScript kb;
+    void TakeDamage(float damageAmount) //Take damage function
+    {
+        Health.damage = Health.damage + damageAmount; //Adds damage to the player health
+        Debug.Log(Health.damage); //Console log of the amount of damage the player has taken
+    }
+
+    private void Update() //Player takedamage function test
+    {
+        if (Input.GetKeyDown(keybind))
+        {
+            TakeDamage(5);
+            kb.Knockback(new Vector3(Health.damage/10,0,0));
+
+        }
+    }
+
+}
