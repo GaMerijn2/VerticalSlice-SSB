12aca16bde2ff4105ba20692c28ae31bd6a629fc
merijn florie <merijn@florie.nl>
2023-11-22 13:12:06 +0100

    Double Jump

---
+++ Vertical-Slice-SSB/Assets/Scripts/PlayerScripts/DoubleJump.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.ShaderGraph.Drawing;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class DoubleJump : MonoBehaviour
+{
+    [SerializeField] private float jumpAmount = 1f;
+    [SerializeField] private float jumpPower = 1f;
+    [SerializeField] private PlayerMovement playerMovement;
+
+    //GroundCheck Objects
+    [SerializeField] private GameObject groundCheckObj;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] public bool groundCheckBool;
+
+    [SerializeField] private float jumpInput;
+    [SerializeField] public string jumpAxis;
+
+    //Multiple Jumps
+    [SerializeField] private bool canJump;
+    [SerializeField] private bool CanDoubleJump;
+    [SerializeField] private bool CanJumpMore = false;
+
+    [SerializeField] string characterName;
+
+    [SerializeField] private bool isJumpButtonDown = false;
+
+    private void Start()
+    {
+        groundCheckObj = GameObject.FindGameObjectWithTag("GroundCheck");
+        characterName = GetComponent<ObjectTags>().characterName;
+    }
+    private void FixedUpdate()
+    {
+        GroundCheck();
+        JumpInputCheck();
+        jumpInput = Input.GetAxis(jumpAxis);
+    }
+
+    private bool GroundCheck()
+    {
+        // checks if the groundcheck is in a small radius of the Ground layer
+        groundCheckBool = Physics.CheckSphere(groundCheckObj.transform.position, 0.5f, groundLayer);
+        return groundCheckBool;
+    }
+    private void JumpInputCheck()
+    {
+        if (jumpInput > 0f && !isJumpButtonDown)
+        {
+            // Button pressed, perform action
+            isJumpButtonDown = true;
+            Jump();
+        }
+
+        if (jumpInput == 0f)
+        {
+            // Button released, reset the flag
+            isJumpButtonDown = false;
+        }
+    }
+
+    private void Jump()
+    {
+
+
+
+        if (characterName == "Kirby")
+        {
+            CanJumpMore = true;
+        }
+
+        if (groundCheckBool)
+        {
+            if (true)
+            {
+                CanDoubleJump = true;
+                playerMovement.rb.velocity = new Vector3(playerMovement.rb.velocity.x, 1 * playerMovement.jumpPower, playerMovement.rb.velocity.z);
+                if (CanDoubleJump)
+                {
+                    CanDoubleJump = false;
+                    playerMovement.rb.velocity = new Vector3(playerMovement.rb.velocity.x, 1 * playerMovement.jumpPower, playerMovement.rb.velocity.z);
+
+                }
+            }
+        }
+    }
+
+}
--- Vertical-Slice-SSB/Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ Vertical-Slice-SSB/Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -5,19 +5,14 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     //Movement Objects
-    [SerializeField] private Rigidbody rb;
+    [SerializeField] public Rigidbody rb;
     [SerializeField] private float moveSpeed;
-    [SerializeField] private float jumpPower;
-
-    //GroundCheck Objects
-    [SerializeField] private GameObject groundCheckObj;
-    [SerializeField] private LayerMask groundLayer;
-    [SerializeField] private bool groundCheckBool;
+    [SerializeField] public float jumpPower;
 
     [SerializeField] private float horizontalInput;
 
     [SerializeField] private string horizontalInputAxis;
-    [SerializeField] private string jumpAxis;
+
     //private float verticalInput;
 
     void Start()
@@ -39,28 +34,13 @@ public class PlayerMovement : MonoBehaviour
     {
         if (rb != null) // checks if the rigidbody isn't null
         {
-            //Updates the GroundCheck bool every time Move() gets called
-            GroundCheck();
-
             //Moves the player Left and Right based on the input
             Vector3 moveInput = new Vector3(horizontalInput, rb.velocity.y,  rb.velocity.z);
             rb.MovePosition(transform.position + moveInput * Time.deltaTime * moveSpeed);
-
-            //lets the player jump when W or Space is pressed
-            if (groundCheckBool == true)
-            {
-                rb.velocity = new Vector3(rb.velocity.x,Input.GetAxis(jumpAxis) * jumpPower, rb.velocity.z);
-            }
         }
         else if(rb == null) // checks if the rigidbody is null
         {
             Debug.LogError("Rigidbody is Null!"); // logs a error message to the console
         }
-    }
-    private bool GroundCheck()
-    {
-        // checks if the groundcheck is in a small radius of the Ground layer
-        groundCheckBool = Physics.CheckSphere(groundCheckObj.transform.position, 0.5f, groundLayer);
-        return groundCheckBool;
     }
 }
