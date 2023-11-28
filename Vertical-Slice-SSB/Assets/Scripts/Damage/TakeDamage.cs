983b6d7991fd03d8d7add695eccde432e3604705
TimothyKuijper <t.kuijper12@gmail.com>
2023-11-28 12:13:45 +0100

    Clean up

--- Vertical-Slice-SSB/Assets/Scripts/Damage/TakeDamage.cs
+++ Vertical-Slice-SSB/Assets/Scripts/Damage/TakeDamage.cs
@@ -8,15 +8,7 @@ public class TakeDamage : MonoBehaviour
     [SerializeField] PlayerHealth Health; //Player health
     public void TakeDamageFun(float damageAmount, float multiplier) //Take damage function
     {
-        Health.damage = Health.damage + damageAmount * multiplier; //Adds damage to the player health
+        Health.damage =+ damageAmount * multiplier; //Adds damage to the player health
+        Debug.Log(Health.damage);
     }
-
-    private void Update() //Player takedamage function test
-    {
-        //if (Input.GetKeyDown(keybind))
-        //{
-            //TakeDamageFun(5); 
-        //}
-    }
-
 }
