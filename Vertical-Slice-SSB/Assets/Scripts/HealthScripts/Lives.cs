e82fd5ddc4c21c8fc62315aabd8db0bbb1ae4459
owen22s <34884@ma-web.nl>
2023-12-07 09:49:45 +0100

    lives gemaakt met imgs inpv txt

--- Vertical-Slice-SSB/Assets/Scripts/HealthScripts/Lives.cs
+++ Vertical-Slice-SSB/Assets/Scripts/HealthScripts/Lives.cs
@@ -1,25 +1,28 @@
-﻿using TMPro;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Lives : MonoBehaviour
 {
     public int startingLives = 3;
     public int currentLives;
     public WinScreen WinScreen; // Reference to the WinScreen script
-    public TextMeshProUGUI livesText;
     public bool isP1;
     public bool isP2;
+    public Image[] liveimgs;
 
     private void Awake()
     {
         currentLives = startingLives;
-        UpdateLivesText();
+
     }
 
     public void DecreaseLives()
     {
         currentLives--;
-        UpdateLivesText();
+        if (currentLives >= 0 && currentLives < liveimgs.Length)
+        {
+            liveimgs[currentLives].gameObject.SetActive(false);
+        }
 
         if (currentLives <= 0)
         {
@@ -35,10 +38,5 @@ public class Lives : MonoBehaviour
             gameObject.SetActive(false);
             currentLives = 0;
         }
-    }
-
-    private void UpdateLivesText()
-    {
-        livesText.text = currentLives.ToString() + " lives left";
     }
 }
