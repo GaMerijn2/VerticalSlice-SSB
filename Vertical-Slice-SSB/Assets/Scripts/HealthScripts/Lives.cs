a9beb86f1c9126f9b0b8681001f356d71bc59d43
merijn florie <merijn@florie.nl>
2023-12-20 09:50:54 +0100

    lives and platforms

--- Vertical-Slice-SSB/Assets/Scripts/HealthScripts/Lives.cs
+++ Vertical-Slice-SSB/Assets/Scripts/HealthScripts/Lives.cs
@@ -5,7 +5,7 @@ public class Lives : MonoBehaviour
 {
     public int startingLives = 3;
     public int currentLives;
-    public WinScreen WinScreen; // Reference to the WinScreen script
+    public WinScreen WinScreen;
     public bool isP1;
     public bool isP2;
     public Image[] liveimgs;
