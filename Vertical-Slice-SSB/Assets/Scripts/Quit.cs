e4339d1cb0990d8c9744ab77e39e57cc9878cb2f
TimothyKuijper <t.kuijper12@gmail.com>
2023-12-20 10:12:00 +0100

    added quiting

---
+++ Vertical-Slice-SSB/Assets/Scripts/Quit.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Quit : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
