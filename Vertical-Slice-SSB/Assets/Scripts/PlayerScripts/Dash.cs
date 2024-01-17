d4b513a690c08e1e3eb3842c9fe6ea2bdf362bb6
merijn florie <merijn@florie.nl>
2024-01-17 10:37:49 +0100

    temp push

--- Vertical-Slice-SSB/Assets/Scripts/PlayerScripts/Dash.cs
+++ Vertical-Slice-SSB/Assets/Scripts/PlayerScripts/Dash.cs
@@ -45,21 +45,20 @@ public class Dash : MonoBehaviour
     {
         canDash = false;
         isDashing = true;
-        //DashSound[0].Play();
+        DashSound[0].Play();
         rb.useGravity = false;
         float originalDrag = rb.drag;
         rb.drag = 0f;
-        animatePlayer.setAnimation("IsDashing", true);
+        animatePlayer.playAnimation("Dash");
 
         rb.velocity = new Vector3(transform.localScale.x * -dashingPower, 0f, -dashingPower);
         trailRenderer.emitting = true;
         yield return new WaitForSeconds(dashingTime);
-        // DashSound[1].Play();
+        DashSound[1].Play();
         trailRenderer.emitting = false;
         isDashing = false;
         rb.useGravity = true;
         rb.drag = originalDrag;
-        animatePlayer.setAnimation("IsDashing", false);
         yield return new WaitForSeconds(dashingCooldown);
         canDash = true;
     }
