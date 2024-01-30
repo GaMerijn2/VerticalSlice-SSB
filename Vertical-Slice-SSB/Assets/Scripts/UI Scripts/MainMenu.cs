1cfa8307733b604d3efa1733b9c138c54a8d7189
TimothyKuijper <t.kuijper12@gmail.com>
2024-01-30 11:58:09 +0100

    mainmenu in progress

---
+++ Vertical-Slice-SSB/Assets/Scripts/UI Scripts/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+
+    [SerializeField] private GameObject button;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
